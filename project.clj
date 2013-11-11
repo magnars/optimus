6a631434f24a45ff0abe1b6d8448ee12aa7b2acb
Christian Johansen <christian@cjohansen.no>
2013-11-11 23:36:24 +0100

    Include the midje plugin
    Otherwise the instructions in the Readme will not work

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [pathetic "0.5.1"]
-                 [clj-time "0.5.1"]]
+                 [clj-time "0.5.1"]
+                 [clj-v8 "0.1.4"]]
   :profiles {:dev {:dependencies [[midje "1.5.0"]]
+                   :plugins [[lein-midje "3.0.0"]]
                    :resource-paths ["test/resources"]}})
-
