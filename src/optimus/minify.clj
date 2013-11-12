2b01dd36b2f964f052871756b93383534adb79c5
Christian Johansen <christian@cjohansen.no>
2013-11-12 07:36:40 +0100

    Pre-build the context with the UglifyJS source

--- src/optimus/minify.clj
+++ src/optimus/minify.clj
@@ -1,10 +1,10 @@
 (ns optimus.minify
   (:require [v8.core :as v8]))
 
-(def uglify (slurp (.getFile (clojure.java.io/resource "uglify.js"))))
-
-(defn minify-js-in-context [context js]
-  (v8/run-script-in-context context (str uglify "
+(defn minify-js-in-uglify-context [context js]
+  "Given a V8 context with the UglifyJS global loaded, minify JS and return the
+results as a string"
+  (v8/run-script-in-context context (str "
 var ast = UglifyJS.parse('" js "');
 ast.figure_out_scope();
 var compressor = UglifyJS.Compressor();
@@ -16,4 +16,13 @@ var stream = UglifyJS.OutputStream();
 compressed.print(stream);
 stream.toString();")))
 
-(def minify-js (partial minify-js-in-context (v8/create-context)))
+(def uglify
+  "The UglifyJS source code, free of dependencies and runnable in a
+stripped context"
+  (slurp (.getFile (clojure.java.io/resource "uglify.js"))))
+
+(defn minify-js [js]
+  "Minify JS with the bundled UglifyJS version"
+  (let [context (v8/create-context)]
+    (v8/run-script-in-context context uglify)
+    (minify-js-in-uglify-context context js)))
