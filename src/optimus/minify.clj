bbab1ae2174afefa653cf9dea83b9c666d02ad2d
Christian Johansen <christian@cjohansen.no>
2013-11-14 00:10:26 +0100

    Make JS mangling of names optional

--- src/optimus/minify.clj
+++ src/optimus/minify.clj
@@ -11,7 +11,7 @@
     (throw (Exception. (clojure.core/subs text 7)))
     text))
 
-(defn- js-minify-code [js]
+(defn- js-minify-code [js {:keys [mangle-names] :or {mangle-names true}}]
   (str "(function () {
     try {
         var ast = UglifyJS.parse('" (escape js) "');
@@ -19,23 +19,29 @@
         var compressor = UglifyJS.Compressor();
         var compressed = ast.transform(compressor);
         compressed.figure_out_scope();
-        compressed.compute_char_frequency();
-        compressed.mangle_names();
-        var stream = UglifyJS.OutputStream();
+        compressed.compute_char_frequency();"
+        (if mangle-names "compressed.mangle_names();" "")
+        "var stream = UglifyJS.OutputStream();
         compressed.print(stream);
         return stream.toString();
     } catch (e) { return 'ERROR: ' + e.message; }
 }());"))
 
-(defn minify-js-in-uglify-context [context js]
+(defn minify-js-in-uglify-context [context js opt]
   "Given a V8 context with the UglifyJS global loaded, minify JS and return the
 results as a string"
-  (throw-v8-exception (v8/run-script-in-context context (js-minify-code js))))
+  (throw-v8-exception (v8/run-script-in-context context (js-minify-code js opt))))
 
 (def uglify
   "The UglifyJS source code, free of dependencies and runnable in a
 stripped context"
   (slurp (clojure.java.io/resource "uglify.js")))
+
+(defn minify-js [js & opt]
+  "Minify JS with the bundled UglifyJS version"
+  (let [context (v8/create-context)]
+    (v8/run-script-in-context context uglify)
+    (minify-js-in-uglify-context context js (first opt))))
 
 (defn- css-minify-code [css]
   (str "
@@ -54,12 +60,6 @@ var console = {
         return translator.translate(cleanInfo(compressed));
     } catch (e) { return 'ERROR: ' + e.message; }
 }());"))
-
-(defn minify-js [js]
-  "Minify JS with the bundled UglifyJS version"
-  (let [context (v8/create-context)]
-    (v8/run-script-in-context context uglify)
-    (minify-js-in-uglify-context context js)))
 
 (defn minify-css-in-csso-context [context css]
   "Given a V8 context with the CSSOCompressor, CSSOTranslator, cleanInfo and
--- test/optimus/minify_test.clj
+++ test/optimus/minify_test.clj
@@ -2,10 +2,31 @@
   (:require [optimus.minify :refer [minify-js minify-css]]
             [midje.sweet :refer [fact => throws]]))
 
-(fact (minify-js "var hello = 2 + 3;") => "var hello=5;")
-(fact (minify-js "var hello = 'Hey';") => "var hello=\"Hey\";")
-(fact (minify-js "var hello = 'Hey' + \n 'there';") => "var hello=\"Heythere\";")
-(fact (minify-js "var hello = ") => (throws Exception))
+(fact
+ "Minifies JS"
+ (minify-js "var hello = 2 + 3;") => "var hello=5;")
+
+(fact
+ "Minifies JS with quotes"
+ (minify-js "var hello = 'Hey';") => "var hello=\"Hey\";")
+
+(fact
+ "Minifies JS with newlines"
+ (minify-js "var hello = 'Hey' + \n 'there';") => "var hello=\"Heythere\";")
+
+(fact
+ "Throws exception on syntax errors"
+ (minify-js "var hello = ") => (throws Exception))
+
+(fact
+ "Mangles names by default"
+ (minify-js "var hmm = (function () { var yoyoyo = 2; return yoyoyo; }());") =>
+ "var hmm=function(){var r=2;return r}();")
+
+(fact
+ "Disable name mangling"
+ (minify-js "var hmm = (function () { var yoyoyo = 2; return yoyoyo; }());" {:mangle-names false}) =>
+ "var hmm=function(){var yoyoyo=2;return yoyoyo}();")
 
 (fact (minify-css "body { color: red; }") => "body{color:red}")
 (fact (minify-css "body {\n    color: red;\n}") => "body{color:red}")
