3f60b8e984ac1a28cda09e0d3a4d57b893f52eee
Magnar Sveen <magnars@gmail.com>
2013-12-30 13:35:03 +0100

    Release 0.13.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject optimus "0.13.3"
+(defproject optimus "0.13.4"
   :description "A Ring middleware for frontend performance optimization."
   :url "http://github.com/magnars/optimus"
   :license {:name "Eclipse Public License"
