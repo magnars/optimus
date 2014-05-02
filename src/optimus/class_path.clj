fd948683b9c601606cf7c617e955dd2296207931
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-05-02 15:51:31 +0200

    use 'clojure.java.io/resource' to find files on classpath.

--- src/optimus/class_path.clj
+++ src/optimus/class_path.clj
@@ -1,37 +1,98 @@
 (ns optimus.class-path
-  (:require [clojure.string :as s])
+  (:require [clojure.string :as s]
+            [clojure.java.io :as io])
   (:import [java.io File]
+           [java.net URL URI]
            [java.util.zip ZipFile ZipEntry]))
 
-(defn class-path-elements []
-  (->> (s/split (System/getProperty "java.class.path" ".") #":")
-       (remove (fn [#^String s] (.contains s "/.m2/")))))
 ;; there are major performance improvements to be gained by not
 ;; traversing the entirety of the class path when running locally and
 ;; picking up files from the class path for every request. Since
 ;; you're not serving files from a .m2 folder in production, this is
 ;; hopefully a safe bet.
 
-(defn get-file-paths [#^File file]
-  (if (.isDirectory file)
-    (mapcat get-file-paths (.listFiles file))
-    [(.getCanonicalPath file)]))
+(defn- ->url
+  [^File f]
+  (vector
+    (.getCanonicalPath f)
+    (-> f (.toURI) (.toURL))))
 
-(defn get-jar-paths [jar]
-  (->> jar
-       (java.util.zip.ZipFile.)
+(defn- ->jar-url
+  [^URL jar ^String path]
+  (let [uri (URI. (str "jar:" (.toURI jar) "!" path))]
+    (vector
+      (or (.getPath uri) path)
+      (.toURL uri))))
+
+(defn- parse-jar-url
+  [^URL url]
+  (let [^String p (.getPath url)
+        idx (.lastIndexOf p "!")]
+    (vector
+      (URL. (if (neg? idx) p (subs p 0 idx)))
+      (when-not (neg? idx)
+        (subs p (inc idx))))))
+
+(defn- jar-contents
+  [^File jar-file]
+  (->> (ZipFile. jar-file)
        (.entries)
        (enumeration-seq)
-       (map (fn [#^ZipEntry e] (.getName e)))))
+       (map #(str "/" (.getName ^ZipEntry %)))
+       (remove #(.endsWith ^String % "/"))
+       (vec)))
 
-(defn get-resource-paths [path]
-  (let [path-plus-slash-length (inc (count path))
-        chop-path #(subs % path-plus-slash-length)
-        file (File. path)]
-    (cond
-     (.isDirectory file) (get-file-paths file)
-     (.exists file) (get-jar-paths file)
-     :else [])))
+(defn- list-jar-contents
+  [^URL url]
+  (let [[jar path] (parse-jar-url url)
+        jar-file (io/file jar)]
+    (when (.isFile jar-file)
+      (let [contents (jar-contents jar-file)]
+        (->> (if path
+               (filter #(.startsWith ^String % path) contents)
+               contents)
+             (map #(->jar-url jar %)))))))
 
-(defn file-paths-on-class-path []
-  (mapcat get-resource-paths (class-path-elements)))
+(defn- list-files
+  [^File f]
+  (when (.exists f)
+    (letfn [(lazy-traverse [^File dir]
+              (lazy-seq
+                (let [fs (.listFiles dir)]
+                  (mapcat
+                    (fn [^File f]
+                      (if (.isDirectory f)
+                        (lazy-traverse f)
+                        [(->url f)]))
+                    fs))))]
+      (if (.isDirectory f)
+        (lazy-traverse f)
+        [(->url f)]))))
+
+(defn- resources
+  [directory-path]
+  (-> (Thread/currentThread)
+      (.getContextClassLoader)
+      (.getResources directory-path)
+      (enumeration-seq)))
+
+(defn resource-urls
+  "Find all files residing in a directory of the given name within _all of the classpath_.
+   Apply the given function to the path string and produce either the path string to use
+   or nil.
+
+   Returns a map of `[path-string -> URL]`."
+  [directory-path path-fn]
+  (let [^File f (io/file directory-path)
+        files (list-files f)]
+    (->> (for [^URL url (resources directory-path)]
+           (case (.getProtocol url)
+             "jar"  (list-jar-contents url)
+             "file" (list-files (io/file url))
+             nil))
+         (apply concat files)
+         (keep
+           (fn [[path url]]
+             (when-let [path' (path-fn path)]
+               [path' url])))
+         (into {}))))
