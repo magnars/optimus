6f683bf94d27a6ff1f69dfca20bc733064aa439c
August Lilleaas <august@augustl.com>
2019-11-17 13:36:34 +0100

    Make digest checksums consistent across *nix and Windows
    The current logic relies on the system encoding to be set to UTF-8, since
    it doesn't work on the bytes of files directly, but rather on their string
    representation.
    
    * Change digest-test so it reads in the test asset using the encoding that
      the test actually expects
    * Change sha-1 and base64-string functions so they both read and write
      UTF-8 contents, which is what the system currently impicitly exlects.

--- test/optimus/digest_test.clj
+++ test/optimus/digest_test.clj
@@ -6,7 +6,7 @@
 (fact (sha-1 "abc") => "a9993e364706816aba3e25717850c26c9cd0d89d")
 (fact (sha-1 "def") => "589c22335a381f122d129225f5c0ba3056ed5811")
 
-(def blank-gif (slurp (io/resource "blank.gif")))
+(def blank-gif (slurp (io/resource "blank.gif") :encoding "UTF-8"))
 
 (fact (base64-string "hello world") => "aGVsbG8gd29ybGQ=")
 (fact (base64-string blank-gif) => "R0lGODlhAQABAO+/vQAAAAAA77+977+977+9Ie+/vQQBAAAAACwAAAAAAQABAAACAUQAOw==")
